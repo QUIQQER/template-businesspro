e2a4150e34916ad863f8c5903822b70625b3f352
Michael <michael.danielczok@gmail.com>
2016-10-04 09:50:27 +0200

    refactor: code style...

--- src/QUI/TemplateBusinessPro/EventHandler.php
+++ src/QUI/TemplateBusinessPro/EventHandler.php
@@ -12,12 +12,13 @@ use QUI;
 
 class EventHandler
 {
-    public static function onProjectConfigSave() {
+    public static function onProjectConfigSave()
+    {
 
         try {
             return QUI\Cache\Manager::clear('quiqqer/templateBusinessPro');
         } catch (QUI\Exception $Exception) {
         }
-        
+
     }
 }
