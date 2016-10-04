0336bd5e257c059c3a9fca62c4da1e0f61471f38
Michael <michael.danielczok@gmail.com>
2016-10-04 09:49:21 +0200

    feat: Event hinzugefügt -> on project config save

---
+++ src/QUI/TemplateBusinessPro/EventHandler.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michael
+ * Date: 04.10.16
+ * Time: 09:37
+ */
+
+namespace QUI\TemplateBusinessPro;
+
+use QUI;
+
+class EventHandler
+{
+    public static function onProjectConfigSave() {
+
+        try {
+            return QUI\Cache\Manager::clear('quiqqer/templateBusinessPro');
+        } catch (QUI\Exception $Exception) {
+        }
+        
+    }
+}
