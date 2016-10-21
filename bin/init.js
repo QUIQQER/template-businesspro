b82fa5fcda78ddf70fcaec70e709683ddfddbbe5
Michael <michael.danielczok@gmail.com>
2016-10-21 09:45:41 +0200

    refactor: init.js aufgeräumt refactor: richtige Abhängigkeiten (peace --> page-transition)

--- bin/init.js
+++ bin/init.js
@@ -1,7 +1,7 @@
 window.addEvent("domready", function () {
     "use strict";
 
-    document.getElements('a img').each(function(Elm) {
+    document.getElements('a img').each(function (Elm) {
         Elm.getParent('a').addClass('image-link');
     });
 
@@ -16,46 +16,6 @@ window.addEvent("domready", function () {
                 console.error("LineNo: " + linenumber);
             });
         });
-
-        /*
-         * Mobile nav:
-         * open or close the sub nav
-         */
-        /*var Nav = document.id("navigation");
-        var NavButtonOpen = document.getElement(".nav-button-open");
-        var NavButtonClose = document.getElement(".nav-button-close");
-        var NavButtonDropDown = document.getElements(".fa-chevron-down");
-
-        var body = document.getElement("body");
-        //Nav.setStyle("height", body.getStyle('height').toInt());
-        //console.log(body.getStyle('height').toInt());
-
-        NavButtonDropDown.addEvent("click", function () {
-            var Li = this.getParent('li');
-            var NavSub = Li.getElement(".page-header-navigation-sub");
-
-            if (NavSub.getStyle("height").toInt() < 1) {
-                NavSub.addClass("nav-toggle-sub");
-                this.addClass("fa-chevron-down-rotate-mobile");
-            }
-            else {
-                NavSub.removeClass("nav-toggle-sub");
-                this.removeClass("fa-chevron-down-rotate-mobile");
-            }
-        });
-
-        NavButtonOpen.addEvent("click", function () {
-            if (!Nav.hasClass("nav-toggle")) {
-                Nav.addClass("nav-toggle");
-            }
-        });
-
-        NavButtonClose.addEvent("click", function () {
-           if (Nav.hasClass("nav-toggle")) {
-               Nav.removeClass("nav-toggle");
-           }
-        });*/
-
     });
 
 });
