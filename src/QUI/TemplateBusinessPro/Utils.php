8776315d9c5e5cfce9955ce43cca54101576e388
Michael <michael.danielczok@gmail.com>
2017-02-08 12:02:04 +0100

    fix: kein caching von Objekten fix: cachen pro Seite refactor: MegaMenu wird nicht gechacht refactor: Breadcrumb Anpassung wegen letzten Änderungen refactor: Headerbild enthält als Titel den Seitentitel fix: hellere Farben funktionieren wieder (\QUI\Utils\Convert()) refactor: code style

--- src/QUI/TemplateBusinessPro/Utils.php
+++ src/QUI/TemplateBusinessPro/Utils.php
@@ -13,6 +13,8 @@ use QUI;
  *
  * @package QUI\TemplateBusinessPro
  * @author www.pcsg.de (Michael Danielczok)
+ *
+ * @return array
  */
 class Utils
 {
@@ -23,7 +25,9 @@ class Utils
     public static function getConfig($params)
     {
         try {
-            return QUI\Cache\Manager::get('quiqqer/templateBusinessPro');
+            return QUI\Cache\Manager::get(
+                'quiqqer/templateBusinessPro/' . $params['Site']->getId()
+            );
         } catch (QUI\Exception $Exception) {
         }
 
@@ -31,7 +35,6 @@ class Utils
 
         /* @var $Project QUI\Projects\Project */
         $Project  = $params['Project'];
-        $Site     = $params['Site'];
         $Template = $params['Template'];
 
         /**
@@ -42,33 +45,33 @@ class Utils
          * own site type
          */
 
-        $showHeader = false;
+        $showHeader     = false;
         $showBreadcrumb = false;
-        $bodyClass = '';
+        $bodyClass      = '';
 
         switch ($Template->getLayoutType()) {
             case 'layout/startPage':
-                $showHeader = $Project->getConfig('templateBusinessPro.settings.showHeaderStartPage');
+                $showHeader     = $Project->getConfig('templateBusinessPro.settings.showHeaderStartPage');
                 $showBreadcrumb = $Project->getConfig('templateBusinessPro.settings.showBreadcrumbStartPage');
-                $bodyClass = 'startpage';
+                $bodyClass      = 'startpage';
                 break;
 
             case 'layout/noSidebar':
-                $showHeader = $Project->getConfig('templateBusinessPro.settings.showHeaderNoSidebar');
+                $showHeader     = $Project->getConfig('templateBusinessPro.settings.showHeaderNoSidebar');
                 $showBreadcrumb = $Project->getConfig('templateBusinessPro.settings.showBreadcrumbNoSidebar');
-                $bodyClass = 'left-sidebar';
+                $bodyClass      = 'left-sidebar';
                 break;
 
             case 'layout/rightSidebar':
-                $showHeader = $Project->getConfig('templateBusinessPro.settings.showHeaderRightSidebar');
+                $showHeader     = $Project->getConfig('templateBusinessPro.settings.showHeaderRightSidebar');
                 $showBreadcrumb = $Project->getConfig('templateBusinessPro.settings.showBreadcrumbRightSidebar');
-                $bodyClass = 'right-sidebar';
+                $bodyClass      = 'right-sidebar';
                 break;
 
             case 'layout/leftSidebar':
-                $showHeader = $Project->getConfig('templateBusinessPro.settings.showHeaderLeftSidebar');
+                $showHeader     = $Project->getConfig('templateBusinessPro.settings.showHeaderLeftSidebar');
                 $showBreadcrumb = $Project->getConfig('templateBusinessPro.settings.showBreadcrumbLeftSidebar');
-                $bodyClass = 'no-sidebar';
+                $bodyClass      = 'no-sidebar';
                 break;
         }
 
@@ -76,92 +79,16 @@ class Utils
 
         $config += array(
             'quiTplType'     => $Project->getConfig('templateBusinessPro.settings.standardType'),
-            'BricksManager'  => \QUI\Bricks\Manager::init(),
             'showHeader'     => $showHeader,
             'showBreadcrumb' => $showBreadcrumb,
-            'settingsCSS' => '<style>' . $settingsCSS . '</style>',
-            'typeClass' => 'type-' . str_replace(array('/', ':'), '-', $Site->getAttribute('type')),
-            'bodyClass' => $bodyClass
+            'settingsCSS'    => '<style>' . $settingsCSS . '</style>',
+            'typeClass'      => 'type-' . str_replace(array('/', ':'), '-', $params['Site']->getAttribute('type')),
+            'bodyClass'      => $bodyClass
         );
 
-
-        /**
-         * Mega menu
-         */
-
-        $searchMobile = '';
-
-        $MegaMenu    = $params['MegaMenu'];
-
-        $types = array(
-            'quiqqer/sitetypes:types/search',
-            'quiqqer/search:types/search'
-        );
-
-        $searchSites = $Project->getSites(array(
-            'where' => array(
-                'type' => array(
-                    'type' => 'IN',
-                    'value' => $types
-                )
-            ),
-            'limit' => 1
-        ));
-
-        if (count($searchSites)) {
-            try {
-                $searchUrl = $searchSites[0]->getUrlRewritten();
-
-                $searchMobile = '<div class="quiqqer-menu-megaMenu-mobile-search hide-on-desktop"
-                                  style="width: auto; font-size: 30px !important;">
-                    <a href="' . $searchUrl . '"
-                    class="header-bar-search-link searchMobile">
-                        <i class="fa fa-search header-bar-search-icon"></i>
-                    </a>
-                </div>';
-            } catch (QUI\Exception $Exception) {
-                QUI\System\Log::addNotice($Exception->getMessage());
-            }
-        }
-
-        $alt = "";
-        if ($Project->getMedia()->getLogoImage()) {
-            $alt = $Project->getMedia()->getLogoImage()->getAttribute('title');
-        }
-        
-
-        $MegaMenu->prependHTML(
-            '<div class="header-bar-inner-logo">
-                <a href="' . URL_DIR . '" class="page-header-logo">
-                <img src="' . $Project->getMedia()->getLogo() . '" alt="' . $alt . '"/></a>
-            </div>'
-        );
-
-        try {
-            QUI::getPackage('quiqqer/search');
-            $Locale = QUI::getLocale();
-
-            $MegaMenu->appendHTML(
-                '<div class="header-bar-suggestSearch hide-on-mobile">
-                    <input type="search" data-qui="package/quiqqer/search/bin/controls/Suggest" 
-                    placeholder="' . $Locale->get('quiqqer/template-businesspro', 'navbar.search.text') . '"/>
-                </div>' .
-                $searchMobile
-            );
-        } catch (QUI\Exception $Exception) {
-            QUI\System\Log::addNotice($Exception->getMessage());
-        }
-
-        $config += array(
-            'MegaMenu' => $MegaMenu
-        );
-
-        $config += array(
-            'Breadcrumb' => $params['Breadcrumb']
-        );
-
+        // set cache
         QUI\Cache\Manager::set(
-            'quiqqer/templateBusinessPro',
+            'quiqqer/templateBusinessPro/' . $params['Site']->getId(),
             $config
         );
 
