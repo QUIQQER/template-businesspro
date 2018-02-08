72445c84005c62f495d14abc47aad5d8773e0416
michael.daniel <michael.danielczok@gmail.com>
2018-02-08 12:00:01 +0100

    refactor: Code style.

--- index.php
+++ index.php
@@ -72,8 +72,9 @@ if ($Project->getConfig('templateBusinessPro.settings.search') != 'hide') {
                     $searchForm .= '<form  action="' . $searchUrl . '" class="header-bar-suggestSearch hide-on-mobile" ';
                     $searchForm .= 'method="get" style="position: relative; right: auto; float: right;">';
                     $searchForm .= '<input type="search" name="search" class="only-input" ' . $dataQui . ' ';
-                    $searchForm .= 'placeholder="' . $Locale->get('quiqqer/template-businesspro',
-                            'navbar.search.text') . '" /></form>';
+                    $searchForm .= 'placeholder="' .
+                                    $Locale->get('quiqqer/template-businesspro', 'navbar.search.text') .
+                                    '" /></form>';
                     break;
                 case 'inputAndIcon':
                     $searchType = 'inputAndIcon';
@@ -82,8 +83,9 @@ if ($Project->getConfig('templateBusinessPro.settings.search') != 'hide') {
                     $searchForm .= '<form  action="' . $searchUrl . '" class="header-bar-suggestSearch hide-on-mobile" method="get">';
                     $searchForm .= '<div class="header-bar-suggestSearch-wrapper">';
                     $searchForm .= '<input type="search" name="search" class="input-and-icon" ' . $dataQui . ' ';
-                    $searchForm .= 'placeholder="' . $Locale->get('quiqqer/template-businesspro',
-                            'navbar.search.text') . '" />';
+                    $searchForm .= 'placeholder="' .
+                                    $Locale->get('quiqqer/template-businesspro', 'navbar.search.text') .
+                                    '" />';
                     $searchForm .= '</div><span class="fa fa-fw fa-search"></span></form>';
                     break;
                 case 'inputAndIconVisible':
@@ -93,8 +95,9 @@ if ($Project->getConfig('templateBusinessPro.settings.search') != 'hide') {
                     $searchForm .= '<form action="' . $searchUrl . '" ';
                     $searchForm .= 'class="header-bar-suggestSearch header-bar-suggestSearch-inputAndIconVisible hide-on-mobile" method="get">';
                     $searchForm .= '<input type="search" name="search" class="input-inputAndIconVisible" ' . $dataQui . ' ';
-                    $searchForm .= 'placeholder="' . $Locale->get('quiqqer/template-businesspro',
-                            'navbar.search.text') . '" />';
+                    $searchForm .= 'placeholder="' .
+                                    $Locale->get('quiqqer/template-businesspro', 'navbar.search.text') .
+                                    '" />';
                     $searchForm .= '<span class="fa fa-fw fa-search"></span></form>';
                     break;
             }
