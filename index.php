298b4516a419d8a54c48e7fa59096bf45bf9ac10
michael.danielczok <michael@pcsg.de>
2020-05-26 10:15:21 +0200

    refactor: code style.

--- index.php
+++ index.php
@@ -67,10 +67,7 @@ if ($Project->getConfig('templateBusinessPro.settings.search') != 'hide'
                     $searchForm .= '<form  action="' . $searchUrl . '" class="header-bar-suggestSearch hide-on-mobile" ';
                     $searchForm .= 'method="get" style="position: relative; right: auto; float: right;">';
                     $searchForm .= '<input type="search" name="search" class="only-input" ' . $dataQui . ' ';
-                    $searchForm .= 'placeholder="' . $Locale->get(
-                            'quiqqer/template-businesspro',
-                            'navbar.search.text'
-                        ) . '" /></form>';
+                    $searchForm .= 'placeholder="' . $Locale->get('quiqqer/template-businesspro', 'navbar.search.text') . '" /></form>';
                     break;
 
                 case 'inputAndIcon':
@@ -80,10 +77,7 @@ if ($Project->getConfig('templateBusinessPro.settings.search') != 'hide'
                     $searchForm .= '<form  action="' . $searchUrl . '" class="header-bar-suggestSearch hide-on-mobile" method="get">';
                     $searchForm .= '<div class="header-bar-suggestSearch-wrapper">';
                     $searchForm .= '<input type="search" name="search" class="input-and-icon" ' . $dataQui . ' ';
-                    $searchForm .= 'placeholder="' . $Locale->get(
-                            'quiqqer/template-businesspro',
-                            'navbar.search.text'
-                        ) . '" />';
+                    $searchForm .= 'placeholder="' . $Locale->get('quiqqer/template-businesspro', 'navbar.search.text') . '" />';
                     $searchForm .= '</div><span class="fa fa-fw fa-search"></span></form>';
                     break;
 
@@ -94,8 +88,7 @@ if ($Project->getConfig('templateBusinessPro.settings.search') != 'hide'
                     $searchForm .= '<form action="' . $searchUrl . '" ';
                     $searchForm .= 'class="header-bar-suggestSearch header-bar-suggestSearch-inputAndIconVisible hide-on-mobile" method="get">';
                     $searchForm .= '<input type="search" name="search" class="input-inputAndIconVisible" ' . $dataQui . ' ';
-                    $searchForm .= 'placeholder="' . $Locale->get('quiqqer/template-businesspro',
-                            'navbar.search.text') . '" />';
+                    $searchForm .= 'placeholder="' . $Locale->get('quiqqer/template-businesspro', 'navbar.search.text') . '" />';
                     $searchForm .= '<span class="fa fa-fw fa-search"></span></form>';
                     break;
             }
